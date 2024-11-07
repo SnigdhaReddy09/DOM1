d74bc1edbea58444fb5ede2904c16bb8ffc86154
SnigdhaReddy09 <93863995+SnigdhaReddy09@users.noreply.github.com>
2024-11-07 09:13:52 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,19 @@
+// 1
+var a=document.getElementById("p")
+function z(){
+    a.textContent="Today's class is intro to DOM"
+    a.style.backgroundColor="red"
+    a.style.fontSize="60px"
+    a.style.borderRadius="50%"
+}
+
+// 2
+function x(){
+    p1.innerHTML='<img id="i1" src="https://images.unsplash.com/photo-1730098482067-5eb95278745a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMnx8fGVufDB8fHx8fA%3D%3D"width="400px" height="300px"><img id="i2" src="https://images.unsplash.com/photo-1730393795034-63122b053734?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNnx8fGVufDB8fHx8fA%3D%3D"width="400px" height="300px"><img id="i3" src="https://images.unsplash.com/photo-1730451306804-f7d3b0a3c4d5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0M3x8fGVufDB8fHx8fA%3D%3D"width="400px" height="300px">'
+    var img1=document.getElementById("i1")
+    img1.style.borderRadius="30px"
+    var img2=document.getElementById("i2")
+    img2.style.borderRadius="100px"
+    var img3=document.getElementById("i3")
+    img3.style.borderRadius="50%"
+}
